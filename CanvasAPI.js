227542b6317111ec071819edf6e62da0e6969032
Jeff Everhart <Jeffeverhart383@gmail.com>
2015-02-06 10:36:46 -0500

    update access token in path

--- CanvasAPI.js
+++ CanvasAPI.js
@@ -5,7 +5,7 @@ var https = require("https");
 
 var options = {
   host: 'canvas.longwood.edu',
-  path: '/api/v1/courses/1011421?access_token=18~y9sXrui2gmEZjUUhTujpKJSvfI6UvIzmCRT8oCavv43734Mhfs7fJXbm6jizKdJA',
+  path: '/api/v1/courses/1011421?access_token=<access_token>',
   method: 'GET'
 };
 
